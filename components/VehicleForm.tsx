707c771a5eddc0151acfc3c9f644e1a28ccdd4b2
Colin Forbes <cjforbes32@gmail.com>
2022-07-02 18:03:50 -0400

    added vehicle form for creating/editing vehicles

--- components/VehicleForm.tsx
+++ components/VehicleForm.tsx
@@ -1,11 +1,74 @@
-import { useState, ChangeEvent } from 'react';
+import { useState, ChangeEvent, useEffect } from 'react';
+import { useSession } from 'next-auth/react';
 import axios from 'axios';
 
 const VehicleForm = () => {
+  const { data: session, status } = useSession();
+
   const [formData, setFormData] = useState({
     make: '',
     model: '',
     year: '',
     mileage: '',
   });
+  const { make, model, year, mileage } = formData;
+
+  useEffect(() => {
+    console.log(status);
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const name = e.target.name,
+      val = e.target.value;
+    setFormData({ ...formData, [name]: val });
+  };
+
+  const handleSubmit = () => {
+    console.log(session, formData);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          name="make"
+          placeholder="eg. Ford/Honda"
+          value={make}
+          onChange={handleChange}
+        />
+
+        <input
+          name="model"
+          placeholder="eg. F-150, Civic"
+          value={model}
+          onChange={handleChange}
+        />
+
+        <input
+          name="year"
+          type="number"
+          min={1886}
+          max={2100}
+          step={1}
+          placeholder="YYYY"
+          value={year}
+          onChange={handleChange}
+        />
+
+        <input
+          name="mileage"
+          type="number"
+          min={0}
+          step={1}
+          placeholder="Mileage"
+          value={mileage}
+          onChange={handleChange}
+        />
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 };
+
+export default VehicleForm;
---
+++ pages/createVehicle.tsx
@@ -0,0 +1,8 @@
+import VehicleForm from '../components/VehicleForm';
+import type { NextPage } from 'next';
+
+const CreateVehicle: NextPage = () => {
+  return <VehicleForm />;
+};
+
+export default CreateVehicle;
