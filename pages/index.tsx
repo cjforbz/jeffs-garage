fdc52f5740e2e7dc4b3ac961bc76f645f9036e13
Colin Forbes <cjforbes32@gmail.com>
2022-08-03 20:11:08 -0400

    deleted userform

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next';
 import { useSession } from 'next-auth/react';
-import UserForm from '../components/UserForm';
 
 const Home: NextPage = () => {
   const { data: session } = useSession();
